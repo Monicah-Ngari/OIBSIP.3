122b5fd85bb0a158eef79a158b4d31201038ce48
Monicah-Ngari <wakarimangari@gmail.com>
2024-03-28 19:31:43 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+function convertTemperature() {
+  event.preventDefault();
+  let temperature = parseFloat(document.getElementById("temperature").value);
+  let unit = document.getElementById("unit").value;
+  let resultElement = document.getElementById("result");
+  let convertedTemperature;
+
+  if (isNaN(temperature)) {
+    resultElement.innerText = "Please enter a valid temperature.";
+    return;
+  }
+
+  if (unit === "celsius") {
+    resultElement.innerText = `The Temperature is ${temperature}°C`;
+  } else if (unit === "fahrenheit") {
+    convertedTemperature = (temperature * 9) / 5 + 32;
+    convertedTemperature = Math.round(convertedTemperature);
+    resultElement.innerText = `The Temperature is ${convertedTemperature}°F`;
+  } else if (unit === "kelvin") {
+    convertedTemperature = temperature + 273.15;
+    convertedTemperature = Math.round(convertedTemperature);
+    resultElement.innerText = `The Temperature is ${convertedTemperature}°K`;
+  }
+}
